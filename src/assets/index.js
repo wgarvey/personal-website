21c965706adcc1238b7f23d5e84e88e108f531f5
William Garvey <wgarvey@gmu.edu>
2019-08-06 20:54:31 -0400

    Added source files

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+// Consolidate images and videos so that they can be imported from the assets directory instead of files individually
+import EvolvedMonaLisaImg from "./evolved-mona.png";
+import EvolvedPenguinImg from "./evolved-penguin.png";
+import MBlasterSampleVid from "./mblaster-sample-cropped.mp4";
+import OrigMonaImg from "./original-mona.jpg";
+import OrigPenguinImg from "./original-penguin.jpg";
+import PhysicsDemo from "./physics-demonstration.mp4";
+import WilliamImg from "./will-garvey-pic.jpg";
+
+export {
+    EvolvedMonaLisaImg,
+    EvolvedPenguinImg,
+    MBlasterSampleVid,
+    OrigMonaImg,
+    OrigPenguinImg,
+    PhysicsDemo,
+    WilliamImg
+}
+
+// export EvolvedMonaLisaImg from "./evolved-mona.png";
+// ^This syntax is better, but results in an error:
+// Support for the experimental syntax 'exportDefaultFrom' isn't currently enabled
